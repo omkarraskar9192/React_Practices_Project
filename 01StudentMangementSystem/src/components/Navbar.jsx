f2e794af2be211f77b4c8db0c764d321d99cc56d
Omkar Raskar <omkarraskar9192@gmail.com>
2026-09-23 18:57:55 +0530

    01 project is add

---
+++ 01StudentMangementSystem/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+navItems = [
+    {
+        slug: '/',
+        name : "Home",
+    },
+    {
+        slug : '/'
+    }
+]
+
+function Navbar() {
+  return (
+
+    <div>
+        <nav>
+            
+        </nav>
+
+
+    </div>
+    
+  )
+}
+
+export default Navbar
---
+++ 01StudentMangementSystem/src/components/StudentBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StudentBar() {
+  return (
+    <div>StudentBar</div>
+  )
+}
+
+export default StudentBar
